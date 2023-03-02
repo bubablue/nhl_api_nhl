1964193901e73ea59c180b0e5ef9df9c302d1c2a
Jose <bubablue00@gmail.com>
2023-03-02 11:32:55 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:3000', 'http://localhost:3000' 'https://national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
-        'national-hockey-league-nhl-bubablue.vercel.app', 'national-hockey-league-nhl-git-main.vercel.app', 'national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app'
+        origins 'localhost:3000', 'http://localhost:3000', 'https://national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
+        'national-hockey-league-nhl-bubablue.vercel.app', 'national-hockey-league-nhl-git-main.vercel.app', 'national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
+        'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl.vercel.app/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*',
+        'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*', 'https://national-hockey-league-nhl.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*',
+        'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*',
+        'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*/*'
         resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
