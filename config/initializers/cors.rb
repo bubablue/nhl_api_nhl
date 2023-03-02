d923d317089489d55853eca85fdfc1a6d91eecc2
Jose <bubablue00@gmail.com>
2023-03-02 10:55:31 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://national-hockey-league-nhl-bubablue.vercel.app/', '
-        https://national-hockey-league-nhl-git-main-bubablue.vercel.app/',
-        'https://national-hockey-league-nhl.vercel.app/', 'https://national-hockey-league-nhl-git-main.vercel.app/',
-        'https://national-hockey-league-nhl-bubablue.vercel.app', 'https://national-hockey-league-nhl-git-main-bubablue.vercel.app',
-        'https://national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-git-main.vercel.app',
-        'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main-bubablue.vercel.app/*',
-        'https://national-hockey-league-nhl.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*',
-        'https://national-hockey-league-nhl-bubablue.vercel.app/*/*', 'https://national-hockey-league-nhl-git-main-bubablue.vercel.app/*/*',
-        'https://*.*', 'https://*', 'http://*.*', 'http://*', 'http://localhost:3000', 'http://localhost:3000/*', 'http://localhost:3000/*/*'
+        origins '*'
         resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
