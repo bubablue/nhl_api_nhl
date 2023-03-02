5758d6648e6de904265ccadd6b7c6e2f374f260f
Jose <bubablue00@gmail.com>
2023-03-02 11:36:46 +0100

    comment out cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,21 +1,21 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-        origins 'localhost:3000', 'http://localhost:3000', 'https://national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
-        'national-hockey-league-nhl-bubablue.vercel.app', 'national-hockey-league-nhl-git-main.vercel.app', 'national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
-        'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl.vercel.app/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*',
-        'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*', 'https://national-hockey-league-nhl.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*',
-        'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*',
-        'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*/*'
-        resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
-    end
-    # allow do
-    #     origins 'https://www.example.com', 'https://example.com'
-    #     resource '*',
-    #     headers: :any,
-    #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    #     credentials: true
-    # end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#     allow do
+#         origins 'localhost:3000', 'http://localhost:3000', 'https://national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
+#         'national-hockey-league-nhl-bubablue.vercel.app', 'national-hockey-league-nhl-git-main.vercel.app', 'national-hockey-league-nhl.vercel.app', 'https://national-hockey-league-nhl-bubablue.vercel.app',
+#         'https://national-hockey-league-nhl-git-main.vercel.app', 'https://national-hockey-league-nhl.vercel.app/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*',
+#         'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*', 'https://national-hockey-league-nhl.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*',
+#         'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*',
+#         'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*/*/*', 'https://national-hockey-league-nhl-bubablue.vercel.app/*/*/*/*'
+#         resource '*',
+#         headers: :any,
+#         methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#         credentials: true
+#     end
+#     # allow do
+#     #     origins 'https://www.example.com', 'https://example.com'
+#     #     resource '*',
+#     #     headers: :any,
+#     #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+#     #     credentials: true
+#     # end
+# end
