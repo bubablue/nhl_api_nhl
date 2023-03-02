30553e0b63eb57922caa39ef71fd18c4b12280d3
Jose <bubablue00@gmail.com>
2023-03-02 09:50:08 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'localhost:3000', '
-        http://localhost:3000'
+        origins 'https://national-hockey-league-nhl-bubablue.vercel.app', '
+        https://national-hockey-league-nhl-git-main-bubablue.vercel.app',
+        'https://national-hockey-league-nhl.vercel.app'
         resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production'
-    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'your-frontend-domain'
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://national-hockey-league-nhl.vercel.app'
 else
     Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
