dfa7690336ba0d0afd012a3be50798f4d5080c75
Jose <bubablue00@gmail.com>
2023-03-02 10:34:42 +0100

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         'https://national-hockey-league-nhl-bubablue.vercel.app/*', 'https://national-hockey-league-nhl-git-main-bubablue.vercel.app/*',
         'https://national-hockey-league-nhl.vercel.app/*', 'https://national-hockey-league-nhl-git-main.vercel.app/*',
         'https://national-hockey-league-nhl-bubablue.vercel.app/*/*', 'https://national-hockey-league-nhl-git-main-bubablue.vercel.app/*/*',
-        'https://*.*', 'https://*', 'http://*.*', 'http://*'
+        'https://*.*', 'https://*', 'http://*.*', 'http://*', 'http://localhost:3000', 'http://localhost:3000/*', 'http://localhost:3000/*/*',
+        ENV.fetch('ALLOWED_ORIGINS') { '*' }
         resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
 if Rails.env === 'production'
     Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://national-hockey-league-nhl.vercel.app'
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://national-hockey-league-nhl-git-main.vercel.app'
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain: 'https://national-hockey-league-nhl-bubablue.vercel.app'
 else
     Rails.application.config.session_store :cookie_store, key: '_authentication_app'
 end
