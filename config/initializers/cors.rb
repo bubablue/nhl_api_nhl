ed4f645c11610bf0c9786791c7dd13ea311b174f
Jose <bubablue00@gmail.com>
2023-03-02 10:07:34 +0100

    update allow origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
         origins 'https://national-hockey-league-nhl-bubablue.vercel.app', '
         https://national-hockey-league-nhl-git-main-bubablue.vercel.app',
-        'https://national-hockey-league-nhl.vercel.app'
+        'https://national-hockey-league-nhl.vercel.app',
+        'https://*'
         resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
