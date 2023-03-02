c9f2ab3f50a19c7ea704aee36dfc985fe1b475dd
Jose <bubablue00@gmail.com>
2023-03-02 17:20:02 +0100

    change to scalingodelete new post

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :registrations, only: [:create]
   resources :players, only: %i[index show create update destroy delete]
-  post :registrations, to: 'registrations#create'
   delete :players, to: 'players#delete'
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
